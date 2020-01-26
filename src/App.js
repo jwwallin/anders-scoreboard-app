08126e4b96f55c825010bdd7fb558e26106a978f
jwwallin <jussi.w.wallin@gmail.com>
2020-01-26 16:28:52 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Scoreboard from 'Scoreboard.js'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"/>
+      <Scoreboard/>
+      <footer className="App-footer"/>
+    </div>
+  );
+}
+
+export default App;
